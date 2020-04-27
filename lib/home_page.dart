d122988a9bea5514697798adae5f8a5d84a4982c
rogeriodelphi <rogeriodelphi@delphi.com>
2020-04-27 01:26:43 -0300

    first commit

---
+++ lib/home_page.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hello Flutter"),
+      ),
+      body: _body(),
+    );
+  }
+
+  _body() {
+    return SingleChildScrollView(
+      child: Container(
+        color: Colors.yellow,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _text(),
+            _pageView(),
+            _buttons(),
+            _text(),
+            _pageView(),
+            _buttons(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Container _pageView() {
+    return Container(
+      margin: EdgeInsets.only(top:20, bottom: 20),
+      height: 300,
+      child: PageView(
+        children: <Widget>[
+          _img("assets/images/dog1.png"),
+          _img("assets/images/dog2.png"),
+          _img("assets/images/dog3.png"),
+          _img("assets/images/dog4.png"),
+          _img("assets/images/dog5.png")
+        ],
+      ),
+    );
+  }
+
+  _buttons() {
+    return Column(
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            _button("ListView"),
+            _button("Page 2"),
+            _button("Page 3")
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            _button("Snack"),
+            _button("Dialog"),
+            _button("Toast")
+          ],
+        )
+      ],
+    );
+  }
+
+  _button(String text) {
+    return RaisedButton(
+        color: Colors.blue,
+        child: Text(
+          text,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+          ),
+        ),
+        onPressed: _onClickOk
+    );
+  }
+
+  void _onClickOk() {
+    print("Clicou no botão!");
+  }
+
+  _img(String img) {
+    return Image.asset(
+      img,
+      fit: BoxFit.cover,
+    );
+  }
+
+  _text() {
+    return Text(
+      "Hello World",
+      style: TextStyle(
+          color: Colors.blue,
+          fontSize: 30,
+          fontWeight: FontWeight.bold,
+          fontStyle: FontStyle.italic,
+          decoration: TextDecoration.underline,
+          decorationColor: Colors.red,
+          decorationStyle: TextDecorationStyle.wavy),
+    );
+  }
+}
